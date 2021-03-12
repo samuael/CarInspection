4c7ed66833fdfb45f7b8ab451e6466bea349a1fd
samuael <samuaeladnew@gmail.com>
2021-03-12 11:09:15 +0300

    adding pgx connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,33 @@
 package main
 
+import (
+	// "log"
+	"context"
+	"os"
+	"sync"
+
+	"github.com/jackc/pgx"
+	pgxplatform "github.com/samuael/Project/CarInspection/platforms/pgx"
+	"github.com/subosito/gotenv"
+)
+
 
 
 func init() {
-	
+	gotenv.Load()
 }
 
-func main(){
+var once sync.Once
+var conn *pgx.Conn
+var connError error
 
-// @Summary 登录
-// @Description 登录
-// @Produce json
-// @Param body body controllers.LoginParams true "body参数"
-// @Success 200 {string} string "ok" "返回用户信息"
-// @Failure 400 {string} string "err_code：10002 参数错误； err_code：10003 校验错误"
-// @Failure 401 {string} string "err_code：10001 登录失败"
-// @Failure 500 {string} string "err_code：20001 服务错误；err_code：20002 接口错误；err_code：20003 无数据错误；err_code：20004 数据库异常；err_code：20005 缓存异常"
-// @Router /user/person/login [post]
+func main(){
+	print(os.Getenv("DB_HOST"))
+	once.Do(func(){
+		conn  , connError = pgxplatform.NewStorage(os.Getenv("DB_USER") , os.Getenv("DB_PASSWORD") , os.Getenv("DB_HOST") , os.Getenv("CAR_INSPECTION_DB_NAME"))
+		if connError != nil {
+			os.Exit(1)
+		}
+	})
+	defer conn.Close(context.Background())
 }
---
+++ platforms/pgx/main.go
@@ -0,0 +1,20 @@
+package pgx
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"github.com/jackc/pgx"
+)
+
+func NewStorage(username, password, host, dbname string) ( *pgx.Conn , error)  {
+	// Preparing the statement 
+	postgresStatment := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", username, password, host, dbname )
+	conn, err := pgx.Connect(context.Background(), postgresStatment )
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		return nil  , err
+	}
+	print(" pgx : DB Connected Succesfuly ... \n")
+	return conn , err 
+}
